d6ec9e76e8cdb4dafeb2e9a4187e3e20430f1df1
DEBADATTA GADANAYAK <38793151+debadattagadanayak@users.noreply.github.com>
2024-12-01 13:29:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -24,9 +24,20 @@ def favicon():
 def validate_cost_center(cost_center_code):
     cost_center = cost_centers.get(cost_center_code)
     if cost_center:
-        return jsonify({"cost_center_code": cost_center_code, **cost_center})
+        return jsonify({
+            "error": "0",
+            "message": "Valid cost center",
+            "costCenter": {
+                "code": cost_center_code,
+                "description": cost_center["description"],
+                "status": cost_center["status"]
+            }
+        })
     else:
-        return jsonify({"error": "Cost center not found", "cost_center_code": cost_center_code}), 404
+        return jsonify({
+            "error": "1",
+            "message": "Invalid cost center"
+        }), 404
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=10000)
